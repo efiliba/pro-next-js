555d391fa7055d8523e7e7bcf09cae8e16276cd7
Eli Filiba <efiliba@hotmail.com>
2024-10-15 22:32:25 +1100

    Add custom Tailwind animate-on-scroll class

---
+++ src/components/AnimateOnScroll/AnimateOnScroll.tsx
@@ -0,0 +1,19 @@
+export function AnimateOnScroll() {
+  return (
+    <div>
+      <div className="animate-on-scroll" />
+      <div className="animate-on-scroll" />
+      <div className="animate-on-scroll" />
+      <div className="animate-on-scroll" />
+      <div className="animate-on-scroll" />
+      <div className="animate-on-scroll" />
+      <div className="animate-on-scroll" />
+      <div className="animate-on-scroll" />
+      <div className="animate-on-scroll" />
+      <div className="animate-on-scroll" />
+      <div className="animate-on-scroll" />
+      <div className="animate-on-scroll" />
+      <div className="animate-on-scroll" />
+    </div>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { Button } from "./ui";
+export * from "./AnimateOnScroll";
 export * from "./Counter";
